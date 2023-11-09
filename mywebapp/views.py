88a98d2db78c52e6abc65bb271e4b86dadf2ebe8
Nicolas <nicoandres397@gmail.com>
2023-11-09 09:38:37 -0300

    Correcciones y agregado de Templates

--- mywebapp/views.py
+++ mywebapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import BusquedaForm
+from .models import TuModelo  # Asegúrate de importar tu modelo adecuado
 
-# Create your views here.
+def formulario_buscar(request):
+    if request.method == 'POST':
+        form = BusquedaForm(request.POST)
+        if form.is_valid():
+            termino_busqueda = form.cleaned_data['termino_busqueda']
+            
+            # Lógica para realizar la búsqueda en la base de datos.
+            resultados = TuModelo.objects.filter(campo1__icontains=termino_busqueda)
+            
+            return render(request, 'resultados_busqueda.html', {'resultados': resultados})
+
+    else:
+        form = BusquedaForm()
+
+    return render(request, 'formulario_buscar.html', {'form': form})
+
+
